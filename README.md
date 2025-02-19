bf5eb10d1b70ee6a46791d06b1c7877078ef634b
abarone <abarone@paloaltonetworks.com>
2025-02-19 10:37:45 -0800

    Initial content upload

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# xsiam-pov-automation
-Automates Cortex XSIAM starter configuration for POVs
+# xsiam-pov
+
+Welcome to the XSIAM POV Automation repository. This automation configures the starter POV configuration for a
+Cortex XSIAM tenant. The starter configuration content is located in a different repository, such as 
+https://github.com/annabarone/xsiam-soc-framework-content.
+
+The python setup script in this repository will:
+
+1. Upload the POVContentPack in the Packs directory 
+2. Create a Cortex/Core REST API Integration Instance using the credential manually created 
+3. Trigger a custom alert 
+4. Grab the alert's ID and output the URL to the alert's workplan
+
+Follow the below steps to run the script: 
+
+### Generate API Key
+
+To begin, you need to generate a Standard XSIAM API Key with Instance Administrator roles. 
+
+1. Get your API key by going to the Cortex XSIAM instance and navigating to:
+        **Settings > Configurations > Integrations > API Keys**
+
+2. Click the button that says **New Key**
+
+3. Configure your key with the following settings: 
+
+    * **Security Level**: Standard
+    * **Role**: Instance Administrator
+    * **Enable Expiration Date**: Optional
+
+4. Copy the key
+
+5. Back on the API Key Table page, keep track of your API Key's ID in the table, and the API URL in the top right corner
+
+
+### Save API Key as a Credential 
+
+1. In XSIAM, navigate to: 
+        **Settings > Configuration > Integrations > Credentials**
+
+2. Click the button that says **New Credential**
+
+3. Configure your credential with the following settings: 
+
+   * **Credential Name**: _Standard XSIAM API Key_
+   * **Username**: Your API Key's ID
+   * **Password**: Your API Key
+
+**Please Note the Credential Name MUST be _Standard XSIAM API Key_ or else the automations won't work.**
+
+4. Click **Save**.
+
+
+### Run Easy Button
+
+1. Please create a `.env` file in the root directory with the follow values: 
+
+```shell
+DEMISTO_BASE_URL=<<<<your API URL>>>>
+DEMISTO_API_KEY=<<<<your API Key previously generated>>>>
+XSIAM_AUTH_ID=<<<<your API Key ID from API Key Table>>>>
+CONTENT_REPO_RAW_LINK=http://raw.githubusercontent.com/annabarone/xsiam-soc-framework-content/refs/heads/master/xsoar_config.json
+```
+
+If you have a different repository with content, please change the CONTENT_REPO_RAW_LINK with your repository's 
+`xsoar_config.json`'s RAW link.
+
+
+2. Create a python venv with `python -m venv venv`
+
+3. Activate the venv with `source ./venv/bin/activate`
+
+4. Install the requirements with `pip install -r requirements.txt`
+
+5. Run the `setup.py` file with `python setup.py`
+
+As a result, a Custom Alert will be created that auto-runs the XSIAM Starter Configuration Setup playbook. This playbook
+grabs the configuration from your CONTENT_REPO on GitHub and installs all content there. 
---
+++ setup.py
@@ -0,0 +1,215 @@
+import json
+import os
+import time
+from typing import Union
+
+import requests
+from click.exceptions import Exit
+from demisto_sdk.commands.upload.upload import upload_content_entity
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEMISTO_BASE_URL = os.getenv("DEMISTO_BASE_URL", "")
+XSIAM_AUTH_ID = os.getenv("XSIAM_AUTH_ID", "")
+DEMISTO_API_KEY = os.getenv("DEMISTO_API_KEY", "")
+CONTENT_REPO_RAW_LINK = os.getenv("CONTENT_REPO_RAW_LINK", "")
+
+headers = {
+    "x-xdr-auth-id": str(XSIAM_AUTH_ID),
+    "Authorization": DEMISTO_API_KEY
+}
+
+
+def upload_initial_content():
+
+    path = os.path.join(os.path.dirname(__file__), "Packs/POVContentPack")
+
+    try:
+        upload_content_entity(
+            input=path,
+            insecure=True,
+        )
+    except Exit as e:
+        if e.exit_code != 0:
+            raise e
+
+
+def create_integration_instances():
+
+    # Grab the Integration Instance Data for POV XSIAM Content Management
+    path = os.path.join(os.path.dirname(__file__), "config_files/integration_instances.json")
+    with open(path, "r") as f:
+        instance_def_list = json.load(f)
+
+    # Reset the server URL
+    for instance_def in instance_def_list:
+        params_list = instance_def["data"]
+        server_url_param = [x for x in params_list if x.get("name") == "url"][0]
+        server_url_param["value"] = DEMISTO_BASE_URL
+
+        # Send integration instance creation request
+        response = requests.put(
+            url=f"{DEMISTO_BASE_URL}/xsoar/public/v1/settings/integration",
+            headers=headers,
+            json=instance_def)
+
+        if response.status_code == 200:
+            json_results = response.json()
+        else:
+            if not "already exists" in response.text:
+                raise Exception(f"Failure: {response.text}")
+
+
+def get_custom_alerts(external_id):
+    """
+    Wrapper around "Get all Alerts" XSIAM API Endpoint, used to search for previously created Custom Alert
+    https://cortex-panw.stoplight.io/docs/cortex-xsiam-1/78woz98r9h224-get-all-alerts
+
+    :param external_id: XSIAM Alert External ID
+    :return:
+    """
+    data = {
+        "request_data": {
+            "filters": [
+                {
+                    "field": "alert_source",
+                    "operator": "in",
+                    "value": ["Custom Alert"]
+                },
+                {
+                    "field": "external_id_list",
+                    "operator": "in",
+                    "value": [external_id]
+                }
+            ],
+            "sort": {
+                "field": "creation_time",
+                "keyword": "asc"
+            }
+        }
+    }
+
+    response = requests.post(
+        url=f"{DEMISTO_BASE_URL}/public_api/v1/alerts/get_alerts",
+        headers=headers,
+        json=data)
+
+    if response.status_code == 200:
+        json_results = response.json()
+
+        return json_results
+    else:
+        raise Exception(f"Failure: {response.text}")
+
+
+def get_alert_id(external_id: str) -> str:
+    """
+    Takes an XSIAM Alert's external ID and associates it with the XSIAM's ID as seen in the UI
+
+    :param external_id: XSIAM alert external UUID
+    :return: XSIAM alert's UI ID
+    """
+    json_results = get_custom_alerts(external_id)
+
+    alert = [x for x in json_results.get("reply").get('alerts') if external_id == x.get('external_id')]
+    if not len(alert) == 1:
+        raise Exception(f"Couldn't find alert with external id {external_id}")
+
+    return alert[0].get("alert_id")
+
+
+def create_custom_alert() -> Union[str, None]:
+    """
+    Wrapper around "Create a Custom Alert" XSIAM API Endpoint
+    https://cortex-panw.stoplight.io/docs/cortex-xsiam-1/7boviliydmbzs-create-a-custom-alert
+
+    :return: UUID string of alert's external ID
+    """
+    data = {
+        "request_data": {
+            "alert": {
+                "vendor": "PANW POV",
+                "product": "PANW POV Starter Configuration",
+                "severity": "High",
+                "category": "POV",
+                "alert_name": "PANW POV Starter Configuration",
+                "description": "Starter Configuration Easy Button for XSIAM POV",
+                "playbook": "XSIAM Starter Configuration Setup",
+                "mitre_defs": {},
+                "pov_github_xsoar_config_file_path": CONTENT_REPO_RAW_LINK,
+            }
+        }
+    }
+
+    response = requests.post(
+        url=f"{DEMISTO_BASE_URL}/public_api/v1/alerts/create_alert",
+        headers=headers,
+        json=data)
+
+    if response.status_code == 200:
+        json_results = response.json()
+        external_id = json_results.get("reply")
+        print(f"----Custom Alert's External ID: {external_id}")
+
+        return external_id
+    else:
+        raise Exception(f"Failure: {response.text}")
+
+
+def trigger_playbook(retries: int=6) -> Union[str, None]:
+    external_id = ""
+
+    # Overall custom alert creation retries -- this will loop if an alert was successfully created but the XSIAM tenant
+    # doesn't log the alert or register it in a given time
+    for _ in range(retries):
+
+        # Initially attempt to kick off custom alert -- sometimes errors because initial content isn't registered yet
+        # Will retry until custom alert is generated -- if not generated, it'll exit
+        for _ in range(retries):
+            try:
+                external_id = create_custom_alert()
+                break
+            except Exception as e:
+                if ("The playbook parameter value XSIAM Starter Configuration Setup is invalid" in str(e) or
+                        "Invalid parameter names: pov_github_xsoar_config_file_path" in str(e)):
+                    print("Waiting for Configuration Playbook to register with XSIAM. Retrying custom alert creation after 90 seconds")
+                    time.sleep(90)
+                    continue
+                else:
+                    print(e)
+                    raise e
+
+        # Raise exception if no external ID after all retries
+        if not external_id:
+            raise Exception("Script could not trigger custom alert. The initial content did not register with the "
+                            "tenant in time.")
+
+        # Try to correlate the external ID to an alert ID
+        for _ in range(retries):
+            try:
+                alert_id = get_alert_id(external_id)
+                return alert_id
+            except Exception as e:
+                if "Couldn't find alert with external" in str(e):
+                    print("Waiting for alert to register with XSIAM. Retrying alert search after 60 seconds.")
+                    time.sleep(60)
+                    continue
+                else:
+                    print(e)
+                    raise e
+
+    raise Exception("Script could not trigger custom alert. XSIAM tenant did not register all custom alerts made.")
+
+
+def main():
+    upload_initial_content()
+    create_integration_instances()
+    time.sleep(15)
+    found_alert_id = trigger_playbook()
+    url = f"{DEMISTO_BASE_URL.replace('api-', '')}/alerts?action:openAlertDetails={found_alert_id}-workPlan"
+    print(f"Alert triggered, view here: {url}")
+
+
+main()
+print("Completed.")
